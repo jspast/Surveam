2504c05db4d1dbd666f17c0a2c487c9cbb20b4d3
jspast <140563347+jspast@users.noreply.github.com>
2024-08-09 09:54:54 -0300

    Função de busca binária em arquivo sequencial e geração de id's com hash

--- src/db/database.py
+++ src/db/database.py
@@ -16,37 +16,91 @@ class Database:
     def create_indexfile(self, name):
         self.indexfiles[name] = open(name + indexfile_ext, 'wb+')
         
-    # ToDo: Busca binária por atributo em arquivo sequencial
-    def search_field(self, filename, value, field):
-        pass
+    # Busca binária por id único em arquivo sequencial, retorna posição do id no arquivo ou posição onde seria inserido
+    def search_id(self, filename, id, id_field, record_size):
+        self.datafiles[filename].seek(0, 2)
+        end = self.datafiles[filename].tell() // record_size
+        start = 0
 
-    # ToDo: Insere registro em arquivo sequencial
+        while start <= end:
+            middle = start + (end - start) // 2
+
+            self.datafiles[filename].seek(id_field.offset + middle * record_size, 0)
+            middle_value = int.from_bytes(self.datafiles[filename].read(id_field.size)[:id_field.size], byteorder='little', signed=False)
+
+            if middle_value < id:
+                start = middle + 1
+            else:
+                end = middle - 1
+
+        return start
+
+    # Devolve true se existe registro com o id no arquivo sequencial e false caso contrário
+    def id_exists(self, filename, id, id_field, record_size):
+        record_num = self.search_id(filename, id, id_field, record_size)
+
+        record = self.get_record(filename, record_num, record_size)
+
+        if record == -1:
+            return False
+
+        if int.from_bytes(record[id_field.offset:id_field.offset + id_field.size], byteorder='little', signed=False) == id:
+            return True
+        else:
+            return False
+
+    # Dada uma posição num no arquivo, devolve o registro nesta posição ou -1 se posição inválida
+    def get_record(self, filename, num, record_size):
+        pos = num * record_size
+        self.datafiles[filename].seek(0, 2)
+        if pos > self.datafiles[filename].tell():
+            return -1
+        else:
+            self.datafiles[filename].seek(pos, 0)
+            return self.datafiles[filename].read(record_size)
+
+    # Retorna o último ID do arquivo ou -1 se for vazio
+    def last_id(self, filename, id_field, record_size):
+        # Vai até o final do arquivo
+        self.datafiles[filename].seek(0, 2)
+
+        # Se o arquivo não for vazio, retorna o último ID
+        if self.datafiles[filename].tell() >= record_size:
+            self.datafiles[filename].seek(id_field.offset - record_size, 2)
+
+            return int.from_bytes(self.datafiles[filename].read(id_field.size)[:id_field.size], byteorder='little', signed=False)
+        
+        # Senão, retorna -1
+        else:
+            return -1
+
+    # Insere registro em arquivo sequencial
     # Ordena record_sort_value pelo atributo sort_field
-    # Indexa com base na tupla index: (type, field, filename)
+    # ToDo: Indexa com base na tupla index: (type, field, filename)
     #   type:   0 = índice numérico
     #           1 = índice patrícia
     #   field:  nome do atributo a ser indexado
     #   filename:   nome do arquivo de índice a ser utilizado
     def insert_record(self, filename, record, record_sort_value, sort_field, index):
 
-        # Vai até o final do arquivo
-        self.datafiles[filename].seek(0, 2)
+        # Se o registro a ser inserido for maior que o último, insere no final do arquivo
+        if record_sort_value >= self.last_id(filename, sort_field, sizeof(record)):
+            self.datafiles[filename].seek(0, 2)
+            self.datafiles[filename].write(record)
 
-        # Se o arquivo não for vazio, compara registro a ser inserido com o último registro
-        if self.datafiles[filename].tell() > 0:
-            self.datafiles[filename].seek(sort_field.offset - sizeof(record), 2)
-
-            # Se o registro a ser inserido for maior, insere após o último registro
-            if record_sort_value >= int.from_bytes(self.datafiles[filename].read(sort_field.size)[:2], byteorder='little', signed=False):
-                self.datafiles[filename].seek(0, 2)
-                self.datafiles[filename].write(record)
-
-            # ToDo: Senão, faz busca binária para achar posição a inserir e insere após mover registros sequentes
-            else:
-                pass
-
-        # Se o arquivo for vazio, insere registro "no final"
+        # Senão, faz busca binária para achar posição a inserir e insere após mover registros sequentes
         else:
+            num = self.search_id(filename, record_sort_value, sort_field, sizeof(record))
+            pos = num * sizeof(record)
+
+            self.datafiles[filename].seek(0, 2)
+            end = self.datafiles[filename].tell()
+
+            self.datafiles[filename].seek(pos, 0)
+            data = self.datafiles[filename].read(end - pos)
+            self.datafiles[filename].seek(pos + sizeof(record), 0)
+            self.datafiles[filename].write(data)
+            self.datafiles[filename].seek(pos, 0)
             self.datafiles[filename].write(record)
         
     # Fecha arquivo de dados
--- src/db/load_data.py
+++ src/db/load_data.py
@@ -1,17 +1,19 @@
 import csv, sys
 
+from hashlib import blake2b
+
 from ctypes import *
 
 from database import Database
 
 class Category(Structure):
     _fields_ = [("id", c_uint8),
-                ("name", c_char * 100),
+                ("name", c_char * 62),
                 ("platform", c_char)]
 
 class Item(Structure):
     _fields_ = [("id", c_uint16),
-                ("name", c_char * 100),
+                ("name", c_char * 61),
                 ("id_category", c_uint8)]
 
 class Moment(Structure):
@@ -20,9 +22,10 @@ class Moment(Structure):
                 ("year", c_uint8)]
 
 class Survey(Structure):
-    _fields_ = [("id_moment", c_uint16),
+    _fields_ = [("id", c_uint32),
                 ("popularity", c_int16),
                 ("change", c_int16),
+                ("id_moment", c_uint16),
                 ("id_item", c_uint16)]
 
 # Converte string de formato "X.XXXX" ou "-X.XXXX" em inteiro
@@ -47,10 +50,6 @@ def load_data(file):
         platform_data = 1
     else:
         platform_data = 0
-    
-    # Inicializa os ids da Categoria e Item com zero
-    id_category = 0
-    id_item = 0 
 
     # Processa linhas do csv
     csvreader = csv.reader(file, delimiter=',')
@@ -64,45 +63,63 @@ def load_data(file):
         
         # Codifica plataforma como caracter
         if platform_data == 1:
-            plataform = row[1][0]
+            platform = row[1][0]
 
             # Define w para pc(Windows)
-            if plataform == 'p':
-                plataform = 'w'
+            if platform == 'p':
+                platform = 'w'
 
         else:
-            plataform = "c"
+            platform = "c"
 
-        # ToDo: processar strings de categoria
-        category = row[1 + platform_data]
-        
+        platform = platform.encode()
 
-        item = row[2 + platform_data]
+        # Processar strings de categoria (Remove texto entre parênteses)
+        category_tmp = row[1 + platform_data].split('(')
+        category = category_tmp[0].strip().encode()
         
+        item = row[2 + platform_data].encode()
 
         # Codifica porcentagens como inteiros
         change = convert_percentage(row[3 + platform_data])
         popularity = convert_percentage(row[4 + platform_data])
 
+        # Calcula id da categoria
+        id_category = blake2b(digest_size=1)
+        id_category.update(category)
+        id_category.update(platform)
+        id_category = id_category.digest()
+
+        # Calcula id do item
+        id_item = blake2b(digest_size=2)
+        id_item.update(item)
+        id_item.update(id_category)
+
+        id_category = int.from_bytes(id_category)
+        id_item = int.from_bytes(id_item.digest())
+
+        # Calcula id do levantamento
+        id_survey = id_moment * 100000 + id_item
+
         # Teste básico:
-        print(str(id_moment) + ' ' + str(month) + ' ' + str(year) + ' ' + plataform + ' ' + category + ' ' + item + ' ' + str(popularity/100) + '% ' + str(change/100) + '%')
+        print(str(id_moment) + ' ' + str(month) + ' ' + str(year) + ' ' + str(platform) + ' ' + category.decode() + ' ' + item.decode() + ' ' + str(popularity/100) + '% ' + str(change/100) + '%')
 
         # Inserir registros no banco de dados
-        survey_record = Survey(id_moment=id_moment, popularity=popularity, change=change, id_item=id_item)
-        db.insert_record("survey", survey_record, survey_record.id_moment, Survey.id_moment, [])
+        if db.id_exists("survey", id_survey, Survey.id, sizeof(Survey)) == False:
+            survey_record = Survey(id=id_survey, id_moment=id_moment, popularity=popularity, change=change, id_item=id_item)
+            db.insert_record("survey", survey_record, survey_record.id, Survey.id, [])
 
-        category_record = Category(id=id_category, name= category,plataform = plataform)
-        db.insert_record("category", category_record, category_record.id,Category.id,[])
+        if db.id_exists("category", id_category, Category.id, sizeof(Category)) == False:
+            category_record = Category(id=id_category, name=category, platform=platform)
+            db.insert_record("category", category_record, category_record.id, Category.id, [])
 
-        item_record = Item(id=id_item, name = item, id_category= id_category)
-        db.insert_record("item", item_record, item_record.id, Item.id, [])
+        if db.id_exists("item", id_item, Item.id, sizeof(Item)) == False:
+            item_record = Item(id=id_item, name=item, id_category=id_category)
+            db.insert_record("item", item_record, item_record.id, Item.id, [])
 
-        moment_record = Moment(id = id_moment,month = month,year = year)
-        db.insert_record("moment", moment_record, moment_record.id, Moment.id,[])
-
-
-        id_category +=1
-        id_item +=1
+        if db.id_exists("moment", id_moment, Moment.id, sizeof(Moment)) == False:
+            moment_record = Moment(id=id_moment, month=month, year=year)
+            db.insert_record("moment", moment_record, moment_record.id, Moment.id, [])
 
     db.close_datafile("survey")
     db.close_datafile("category")
